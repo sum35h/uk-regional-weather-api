ba2676f1382abde1156ca8fd8ffdbec7fb86d807
sum35sh <sumesh.6361@gmail.com>
2022-01-10 22:38:30 +0530

    models for data

--- climate_science/data/models.py
+++ climate_science/data/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class MonthlyData(models.Model):
+    region = models.CharField(max_length=64, db_index=True)
+    time = models.DateField()
+    parameter = models.CharField(max_length=64, db_index=True)
+    value = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return self.region + ' - ' + str(self.time) + ' - ' + self.parameter + ': ' + str(self.value)
+
+class SeasonalData(models.Model):
+    region = models.CharField(max_length=64, db_index=True)
+    time = models.DateField()
+    parameter = models.CharField(max_length=64, db_index=True)
+    value = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return self.region + ' - ' + str(self.time) + ' - ' + self.parameter + ': ' + str(self.value)
+
+class AnnualData(models.Model):
+    region = models.CharField(max_length=64, db_index=True)
+    time = models.DateField()
+    parameter = models.CharField(max_length=64, db_index=True)
+    value = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return self.region + ' - ' + str(self.time) + ' - ' + self.parameter + ': ' + str(self.value)
